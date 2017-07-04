ab7219756c493c20d9c02dc5fd5ceaaebe3a49bf
Shourai <10200748+Shourai@users.noreply.github.com>
2017-07-04 17:08:29 +0200

    Backup files moved to ~/.saves folder

--- emacs.d/emacs.el
+++ emacs.d/emacs.el
@@ -7,6 +7,15 @@
 
 (add-hook 'focus-out-hook 'save-all)
 
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
