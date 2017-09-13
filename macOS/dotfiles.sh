7b51ecd37129a01828a2fe38f4790b6770b82412
Shourai <10200748+Shourai@users.noreply.github.com>
2017-09-13 13:31:04 +0200

    added ranger config

--- macOS/dotfiles.sh
+++ macOS/dotfiles.sh
@@ -6,3 +6,4 @@ ln -s ~/Documents/github/dotfiles/emacs.d ~/.emacs.d
 ln -s ~/Documents/github/dotfiles/weechat.conf ~/.weechat.conf
 ln -s ~/Documents/github/dotfiles/ledgerrc ~/.ledgerrc
 ln -s ~/Documents/github/dotfiles/latexmkrc ~/.latexmkrc
+ln -s ~/Documents/github/dotfiles/ranger ~/.config/ranger
