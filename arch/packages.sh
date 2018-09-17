5d51ab5d9d62667ae5605dbba2870bcc9c5bcc4f
Shourai <10200748+Shourai@users.noreply.github.com>
2018-09-17 14:27:15 +0200

    Added new AUR helper

--- arch/packages.sh
+++ arch/packages.sh
@@ -1,25 +1,24 @@
-# Add Yaourt to pacman
-yaourt="
-[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/\$arch"
-
-echo "$yaourt" | sudo tee -a /etc/pacman.conf > /dev/null
+# Install AUR helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 # update package list
 sudo pacman -Syu --noconfirm
 
 # Install packages
-sudo pacman -S yaourt --noconfirm
 sudo pacman -S i3 --noconfirm
 sudo pacman -S xorg --noconfirm
 sudo pacman -S xorg-xinit --noconfirm
+sudo pacman -S dmenu --noconfirm
+sudo pacman -S i3blocks --noconfirm
+sudo pacman -S archey3 --noconfirm
+sudo pacman -S ttf-dejavu --noconfirm
 sudo pacman -S openssh --noconfirm
-sudo pacman -S sudo --noconfirm
 sudo pacman -S python3 --noconfirm
-sudo pacman -S python-pip --noconfirm
+# sudo pacman -S python-pip --noconfirm
 sudo pacman -S tmux --noconfirm
-sudo pacman -S vim --noconfirm
+# sudo pacman -S vim --noconfirm
 sudo pacman -S neovim --noconfirm
 sudo pacman -S zsh --noconfirm
 sudo pacman -S zsh-syntax-highlighting --noconfirm
@@ -28,24 +27,18 @@ sudo pacman -S ledger --noconfirm
 sudo pacman -S fasd --noconfirm
 sudo pacman -S fzf --noconfirm
 sudo pacman -S rxvt-unicode --noconfirm
-sudo pacman -S dmenu --noconfirm
-sudo pacman -S i3blocks --noconfirm
-sudo pacman -S archey3 --noconfirm
-sudo pacman -S ttf-dejavu --noconfirm
 sudo pacman -S sysstat --noconfirm
 sudo pacman -S acpi --noconfirm
 sudo pacman -S feh --noconfirm
 sudo pacman -S htop --noconfirm
 sudo pacman -S pulseaudio --noconfirm
 sudo pacman -S alsa-utils --noconfirm
-sudo pacman -S pulseaudio --noconfirm
-sudo pacman -S alsa-utils --noconfirm
 #sudo pacman -S pulseaudioalsa-utils --noconfirm
 sudo pacman -S xorg-backlight --noconfirm
 sudo pacman -S xf86-video-intel --noconfirm
 sudo pacman -S mpv --noconfirm
-yaourt -S ttf-font-awesome --noconfirm
-yaourt -S i3lock-color-git --noconfirm
+yay -S ttf-font-awesome --noconfirm
+yay -S i3lock-color-git --noconfirm
 
 # Do a cleanup
 sudo pacman -Sc --noconfirm
