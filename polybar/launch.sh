acdbad8fe0efcadba1730109aa2f729c8c302502
Shourai <10200748+Shourai@users.noreply.github.com>
2019-05-07 17:32:20 +0200

    Updates to window manager
    bspwmrc updated to three monitor layout and twelve workspaces.
    xautolock has been added, will now first lock after 10 minutes and then
    after 10 more minutes suspend the system.
    
    Polybar updated to accomodate multimonitors.
    
    sxhkd updated to accomodate workspace 11 and 12.
    Also added shortcut to show/hide polybar.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+
+if pgrep -x "polybar" > /dev/null; then
+    killall polybar
+fi
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
