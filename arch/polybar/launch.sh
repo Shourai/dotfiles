99e6a8ca1a07296444faad4da2feb591b7c08357
Shourai <10200748+Shourai@users.noreply.github.com>
2019-05-08 10:40:19 +0200

    clean up dotfiles folder by moving files to corresponding folder

---
+++ arch/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+
+if pgrep -x "polybar" > /dev/null; then
+    killall polybar
+fi
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Enumerate multi monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload mybar &
+done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# polybar mybar &
+
+echo "Polybar launched..."
