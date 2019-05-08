b631d110fa9d2b9368e032476b15ded8b00fb332
Shourai <10200748+Shourai@users.noreply.github.com>
2019-05-08 10:43:42 +0200

    Clean up dot files by moving files to corresponding folder

--- macOS/dotfiles.sh
+++ macOS/dotfiles.sh
@@ -1,12 +1,12 @@
 # Link dotfiles
 
 mkdir -p ~/.config
-ln -s ~/Documents/github/dotfiles/vimrc ~/.vimrc
-ln -s ~/Documents/github/dotfiles/zshrc ~/.zshrc
+ln -s ~/Documents/github/dotfiles/nvim/init.vim ~/.vimrc
+ln -s ~/Documents/github/dotfiles/zsh/zshrc ~/.zshrc
 
 mkdir -p ~/.zsh/functions
-ln -s ~/Documents/github/dotfiles/pure.zsh ~/.zsh/functions/prompt_pure_setup
-ln -s ~/Documents/github/dotfiles/async.zsh ~/.zsh/functions/async
+ln -s ~/Documents/github/dotfiles/zsh/pure.zsh ~/.zsh/functions/prompt_pure_setup
+ln -s ~/Documents/github/dotfiles/zsh/async.zsh ~/.zsh/functions/async
 
 ln -s ~/Documents/github/dotfiles/emacs.d ~/.emacs.d
 ln -s ~/Documents/github/dotfiles/weechat.conf ~/.weechat.conf
@@ -18,12 +18,12 @@ ln -s ~/Documents/github/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/Documents/github/dotfiles/neomutt ~/.config/neomutt
 
 if [ "$(uname)" = "Linux" ]; then
-    ln -s ~/Documents/github/dotfiles/xinitrc ~/.xinitrc
-    ln -s ~/Documents/github/dotfiles/xresources ~/.xresources
-    ln -s ~/Documents/github/dotfiles/i3 ~/.config/
-    ln -s ~/Documents/github/dotfiles/urxvt ~/.urxvt
-    ln -s ~/Documents/github/dotfiles/compton.conf ~/.config
-    ln -s ~/Documents/github/dotfiles/bspwm ~/.config
-    ln -s ~/Documents/github/dotfiles/sxhkd ~/.config
-    ln -s ~/Documents/github/dotfiles/polybar ~/.config
+    ln -s ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
+    ln -s ~/Documents/github/dotfiles/arch/xresources ~/.xresources
+    ln -s ~/Documents/github/dotfiles/arch/i3 ~/.config/
+    ln -s ~/Documents/github/dotfiles/arch/urxvt ~/.urxvt
+    ln -s ~/Documents/github/dotfiles/arch/compton.conf ~/.config
+    ln -s ~/Documents/github/dotfiles/arch/bspwm ~/.config
+    ln -s ~/Documents/github/dotfiles/arch/sxhkd ~/.config
+    ln -s ~/Documents/github/dotfiles/arch/polybar ~/.config
 fi
