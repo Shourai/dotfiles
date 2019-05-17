7d383a0ee7665ad6db569c846f3ab6de9ed8a0a9
Shourai <10200748+Shourai@users.noreply.github.com>
2019-05-17 17:28:02 +0200

    Force symlinks to be overwritten

--- macOS/dotfiles.sh
+++ macOS/dotfiles.sh
@@ -1,29 +1,29 @@
 # Link dotfiles
 
 mkdir -p ~/.config
-ln -s ~/Documents/github/dotfiles/nvim/init.vim ~/.vimrc
-ln -s ~/Documents/github/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/Documents/github/dotfiles/nvim/init.vim ~/.vimrc
+ln -sf ~/Documents/github/dotfiles/zsh/zshrc ~/.zshrc
 
 mkdir -p ~/.zsh/functions
-ln -s ~/Documents/github/dotfiles/zsh/pure.zsh ~/.zsh/functions/prompt_pure_setup
-ln -s ~/Documents/github/dotfiles/zsh/async.zsh ~/.zsh/functions/async
+ln -sf ~/Documents/github/dotfiles/zsh/pure.zsh ~/.zsh/functions/prompt_pure_setup
+ln -sf ~/Documents/github/dotfiles/zsh/async.zsh ~/.zsh/functions/async
 
-ln -s ~/Documents/github/dotfiles/emacs.d ~/.emacs.d
-ln -s ~/Documents/github/dotfiles/weechat.conf ~/.weechat.conf
-ln -s ~/Documents/github/dotfiles/ledgerrc ~/.ledgerrc
-ln -s ~/Documents/github/dotfiles/latexmkrc ~/.latexmkrc
-ln -s ~/Documents/github/dotfiles/ranger ~/.config/ranger
-ln -s ~/Documents/github/dotfiles/nvim ~/.config/nvim
-ln -s ~/Documents/github/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/Documents/github/dotfiles/neomutt ~/.config/neomutt
+ln -sf ~/Documents/github/dotfiles/emacs.d ~/.emacs.d
+ln -sf ~/Documents/github/dotfiles/weechat.conf ~/.weechat.conf
+ln -sf ~/Documents/github/dotfiles/ledgerrc ~/.ledgerrc
+ln -sf ~/Documents/github/dotfiles/latexmkrc ~/.latexmkrc
+ln -sf ~/Documents/github/dotfiles/ranger ~/.config/ranger
+ln -sf ~/Documents/github/dotfiles/nvim ~/.config/nvim
+ln -sf ~/Documents/github/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/Documents/github/dotfiles/neomutt ~/.config/neomutt
 
 if [ "$(uname)" = "Linux" ]; then
-    ln -s ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
-    ln -s ~/Documents/github/dotfiles/arch/xresources ~/.xresources
-    ln -s ~/Documents/github/dotfiles/arch/i3 ~/.config/
-    ln -s ~/Documents/github/dotfiles/arch/urxvt ~/.urxvt
-    ln -s ~/Documents/github/dotfiles/arch/compton.conf ~/.config
-    ln -s ~/Documents/github/dotfiles/arch/bspwm ~/.config
-    ln -s ~/Documents/github/dotfiles/arch/sxhkd ~/.config
-    ln -s ~/Documents/github/dotfiles/arch/polybar ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
+    ln -sf ~/Documents/github/dotfiles/arch/xresources ~/.xresources
+    ln -sf ~/Documents/github/dotfiles/arch/i3 ~/.config/
+    ln -sf ~/Documents/github/dotfiles/arch/urxvt ~/.urxvt
+    ln -sf ~/Documents/github/dotfiles/arch/compton.conf ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/bspwm ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/sxhkd ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/polybar ~/.config
 fi
