65a7a7323a35c63e5ad4dced29fa2277139f7240
Shourai <10200748+Shourai@users.noreply.github.com>
2020-03-05 13:04:42 +0100

    Update packages

--- arch/packages.sh
+++ arch/packages.sh
@@ -7,34 +7,35 @@ makepkg -si --noconfirm
 sudo pacman -Syu --noconfirm
 
 # Install packages
+# sudo pacman -S alsa-utils --noconfirm
+# sudo pacman -S i3-gaps --noconfirm
+# sudo pacman -S i3blocks --noconfirm
+# sudo pacman -S ledger --noconfirm
+# sudo pacman -S pavucontrol --noconfirm
+# sudo pacman -S rxvt-unicode --noconfirm
 sudo pacman -S acpi --noconfirm
 sudo pacman -S archey3 --noconfirm
+sudo pacman -S bspwm
 sudo pacman -S dmenu --noconfirm
 sudo pacman -S emacs --noconfirm
 sudo pacman -S fasd --noconfirm
 sudo pacman -S feh --noconfirm
 sudo pacman -S fzf --noconfirm
 sudo pacman -S htop --noconfirm
-# sudo pacman -S i3-gaps --noconfirm
-# sudo pacman -S i3blocks --noconfirm
-sudo pacman -S bspwm
-#sudo pacman -S ledger --noconfirm
+sudo pacman -S mesa --noconfirm
 sudo pacman -S mpv --noconfirm
 sudo pacman -S neovim --noconfirm
 sudo pacman -S openssh --noconfirm
-#sudo pacman -S pavucontrol --noconfirm
 sudo pacman -S picom --noconfirm
 sudo pacman -S pulseaudio --noconfirm
 sudo pacman -S python-pip --noconfirm
 sudo pacman -S python3 --noconfirm
-#sudo pacman -S rxvt-unicode --noconfirm
-sudo pacman -S sysstat --noconfirm
 sudo pacman -S sxhkd --noconfirm
+sudo pacman -S sysstat --noconfirm
 sudo pacman -S the_silver_searcher --noconfirm
 sudo pacman -S tmux --noconfirm
 sudo pacman -S ttf-dejavu --noconfirm
 sudo pacman -S xclip --noconfirm
-sudo pacman -S xf86-video-intel --noconfirm
 sudo pacman -S xorg --noconfirm
 sudo pacman -S xorg-backlight --noconfirm
 sudo pacman -S xorg-xinit --noconfirm
@@ -45,4 +46,3 @@ yay -S otf-font-awesome --noconfirm
 yay -S betterlockscreen --noconfirm
 yay -S flashfocus-git --noconfirm
 yay -S polybar --noconfirm
-# sudo pacman -S alsa-utils --noconfirm
