8302925c99dde0f8339e85a811da86d180516579
Shourai <10200748+Shourai@users.noreply.github.com>
2020-03-05 15:05:04 +0100

    Link bspwm script

--- macOS/dotfiles.sh
+++ macOS/dotfiles.sh
@@ -37,4 +37,5 @@ if [ "$(uname)" = "Linux" ]; then
     ln -sf ~/Documents/github/dotfiles/arch/sxhkd ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/polybar ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/fontconfig ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/bspwm/scripts/bspwm_resize ~/.local/bin
 fi
