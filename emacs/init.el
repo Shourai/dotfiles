5f82c8ee2075c6ba5fb6392c7b17651c1f06a90b
Shourai <10200748+Shourai@users.noreply.github.com>
2020-08-17 12:18:49 +0200

    Comment out package initialize

--- emacs/init.el
+++ emacs/init.el
@@ -5,7 +5,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
 
 (setq gc-cons-threshold (* 50 1000 1000))
 
