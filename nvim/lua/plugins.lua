859180c7d08dd8dabdea48a69f8ffa2b534e7a0a
Shourai <10200748+Shourai@users.noreply.github.com>
2021-07-08 09:04:05 +0200

    Add easy align plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,17 +8,21 @@ return require('packer').startup(function()
   use 'hrsh7th/nvim-compe'
 
   -- Telescope
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
 
   -- Treesitter
   use 'nvim-treesitter/nvim-treesitter'
+
   -- Theme
   use 'NTBBloodbath/doom-one.nvim'
   use 'navarasu/onedark.nvim'
 
   -- Autopairs
   use 'windwp/nvim-autopairs'
+
+  use 'junegunn/vim-easy-align'
 end)
 
