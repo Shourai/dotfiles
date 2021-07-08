4f8b7cd5649af3c15f40a9f83b4159a006790e0c
Shourai <10200748+Shourai@users.noreply.github.com>
2021-07-08 09:04:35 +0200

    Switch to window/local/buffer options

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,29 +1,31 @@
 --- Helpers ---
 local cmd = vim.cmd
-local opt = vim.opt
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
 
 -- Settings --
-opt.backup = false -- don't create backup file
-opt.clipboard = "unnamedplus" -- allow neovim to access the system clipboard
-opt.completeopt = { "menuone", "noselect" }
-opt.conceallevel = 0 -- so that `` is visible in markdown files
-opt.fileencoding = "utf-8" -- the encoding written to a file
-opt.ignorecase = true -- ignore case in search pattern
-opt.smartcase = true -- smart case
-opt.mouse = "a" -- allow the mouse to be used in neovim
+o.backup = false -- don't create backup file
+o.clipboard = "unnamedplus" -- allow neovim to access the system clipboard
+o.completeopt = "menuone,noselect"
+wo.conceallevel = 0 -- so that `` is visible in markdown files
+bo.fileencoding = "utf-8" -- the encoding written to a file
+o.ignorecase = true -- ignore case in search pattern
+o.smartcase = true -- smart case
+o.mouse = "a" -- allow the mouse to be used in neovim
 
-opt.splitbelow = true -- force all horizontal splits to go below current window
-opt.splitright = true -- force all vertical splits to go to the right of current window
+o.splitbelow = true -- force all horizontal splits to go below current window
+o.splitright = true -- force all vertical splits to go to the right of current window
 
-opt.swapfile = false -- creates a swapfile
-opt.expandtab = true -- convert tabs to spaces
-opt.tabstop = 2
-opt.shiftwidth = 2
+bo.swapfile = false -- creates a swapfile
+bo.expandtab = true -- convert tabs to spaces
+bo.tabstop = 2
+bo.shiftwidth = 2
 
-opt.cursorline = true -- highlight the current line
+wo.cursorline = true -- highlight the current line
 
-opt.number = true  -- set numbered lines
-opt.relativenumber = true -- set relative numbered lines
+wo.number = true  -- set numbered lines
+wo.relativenumber = true -- set relative numbered lines
 
-vim.api.nvim_command("autocmd FocusLost * silent! :wa") -- write file when focus is lost
-opt.autowriteall = true -- write the contents of the file when jumping between jumppoints
+cmd("autocmd FocusLost * silent! :wa") -- write file when focus is lost
+o.autowriteall = true -- write the contents of the file when jumping between jumppoints
