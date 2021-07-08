864e6ae1d8d3cf8c20212228562e9e457f5667c1
Shourai <10200748+Shourai@users.noreply.github.com>
2021-07-08 09:47:00 +0200

    Add tmux.nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,6 +23,32 @@ return require('packer').startup(function()
   -- Autopairs
   use 'windwp/nvim-autopairs'
 
+  -- Easy align
   use 'junegunn/vim-easy-align'
+
+  -- Tmux
+  use({
+    "aserowy/tmux.nvim",
+    config = function()
+        require("tmux").setup({
+            -- overwrite default configuration
+            -- here, e.g. to enable default bindings
+            copy_sync = {
+                -- enables copy sync and overwrites all register actions to
+                -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+                enable = true,
+            },
+            navigation = {
+                -- enables default keybindings (C-hjkl) for normal mode
+                enable_default_keybindings = true,
+            },
+            resize = {
+                -- enables default keybindings (A-hjkl) for normal mode
+                enable_default_keybindings = true,
+            }
+        })
+    end
+    })
+
 end)
 
