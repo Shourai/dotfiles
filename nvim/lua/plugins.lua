4486505b89509e43c733bec2a5726bcea2ced11f
Shourai <10200748+Shourai@users.noreply.github.com>
2021-08-14 09:24:16 +0200

    Add copy_sync functionality to tmux.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -53,6 +53,13 @@ return require('packer').startup(function()
     "aserowy/tmux.nvim",
     config = function()
       require("tmux").setup({
+        copy_sync = {
+          enable = true,
+          redirect_to_clipboard = true,
+          register_offset = 0,
+          sync_clipboard = true,
+          sync_deletes = true,
+        },
         navigation = { enable_default_keybindings = true },
         resize = { enable_default_keybindings = true } })
       end
