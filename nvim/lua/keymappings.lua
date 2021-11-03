89ac97e39a7e47938d2586a122acae1c953f29e6
Shourai <10200748+Shourai@users.noreply.github.com>
2021-11-03 10:27:41 +0100

    Move leader key mapping into settings file

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,7 +1,3 @@
---
-
-vim.g.mapleader = " "
-
 -- better window movement
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
