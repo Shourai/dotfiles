dbf401eae401e240c83e4939381aee1d01a53f89
Shourai <10200748+Shourai@users.noreply.github.com>
2021-12-30 13:52:23 +0100

    Remove unused dotfile symlinks

--- arch/dotfiles.sh
+++ arch/dotfiles.sh
@@ -9,29 +9,11 @@ ln -sf ~/Documents/github/dotfiles/zsh/async.zsh ~/.zsh/functions/async
 ln -sf ~/Documents/github/dotfiles/zsh/LS_COLORS ~/.zsh/LS_COLORS
 
 ln -sf ~/Documents/github/nvim ~/.config/nvim
-ln -sf ~/Documents/github/dotfiles/emacs ~/.config/emacs
 ln -sf ~/Documents/github/dotfiles/tmux ~/.config/tmux
 ln -sf ~/Documents/github/dotfiles/git ~/.config
 
-echo "Do you wish to add ledgerrc, latexmkrc and ranger? (y/n)"
-read -r answer
-
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    ln -sf ~/Documents/github/dotfiles/ledgerrc ~/.ledgerrc
-    ln -sf ~/Documents/github/dotfiles/latexmkrc ~/.latexmkrc
-    ln -sf ~/Documents/github/dotfiles/ranger ~/.config/ranger
-else
-    echo "Skipping lederrc, latexmkrc, ranger"
-fi
-
-# ln -sf ~/Documents/github/dotfiles/neomutt ~/.config/neomutt
-# ln -sf ~/Documents/github/dotfiles/weechat.conf ~/.weechat.conf
-
 if [ "$(uname)" = "Linux" ]; then
     ln -sf ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
-    # ln -sf ~/Documents/github/dotfiles/arch/xresources ~/.xresources
-    # ln -sf ~/Documents/github/dotfiles/arch/i3 ~/.config/
-    # ln -sf ~/Documents/github/dotfiles/arch/urxvt ~/.urxvt
     ln -sf ~/Documents/github/dotfiles/arch/picom.conf ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/bspwm ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/sxhkd ~/.config
