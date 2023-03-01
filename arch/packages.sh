3a63a610b23c53bfab4723750027a8b58e09bfce
Shourai <10200748+Shourai@users.noreply.github.com>
2023-03-01 12:32:08 +0100

    Add polkit

--- arch/packages.sh
+++ arch/packages.sh
@@ -24,6 +24,7 @@ sudo pacman -S openssh --noconfirm
 sudo pacman -S picom --noconfirm
 sudo pacman -S pipewire --noconfirm
 sudo pacman -S pipewire-pulse --noconfirm
+sudo pacman -S polkit --noconfirm
 sudo pacman -S python-pip --noconfirm
 sudo pacman -S python3 --noconfirm
 sudo pacman -S rofi --noconfirm
