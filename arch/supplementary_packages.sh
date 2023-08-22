d5d203e723f145306f91a392d06c5ae161e46911
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-22 21:02:31 +0200

    Add supplementary packages

---
+++ arch/supplementary_packages.sh
@@ -0,0 +1,6 @@
+# Install supplementary packages
+sudo pacman -S chromium --noconfirm
+sudo pacman -S firefox --noconfirm
+sudo pacman -S npm --noconfirm
+yay -S 1password --noconfirm
+yay -S brave-bin --noconfirm
