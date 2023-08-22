87f7c8b6d01840bab2c7ee50d805949effb51c9a
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-22 21:28:46 +0200

    Add seahorse package

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -2,5 +2,6 @@
 sudo pacman -S chromium --noconfirm
 sudo pacman -S firefox --noconfirm
 sudo pacman -S npm --noconfirm
+sudo pacman -S seahorse --noconfirm
 yay -S 1password --noconfirm
 yay -S brave-bin --noconfirm
