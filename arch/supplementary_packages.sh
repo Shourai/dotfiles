e2b891385450d5e07749ec30e21e92b03aa4d6af
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-22 21:52:23 +0200

    Add nnn plugins

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -5,3 +5,12 @@ sudo pacman -S npm --noconfirm
 sudo pacman -S seahorse --noconfirm
 yay -S 1password --noconfirm
 yay -S brave-bin --noconfirm
+
+
+# Add nnn plugins
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+
+# Install tabbed for nnn tabbed to work
+git clone https://git.suckless.org/tabbed ~/Documents/github/tabbed
+cd ~/Documents/github/tabbed || exit
+sudo make install
