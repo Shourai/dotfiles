4f34d318b38325ffc21c119b49a6f167ee3f3c47
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-22 21:02:24 +0200

    Add lua package

--- arch/packages.sh
+++ arch/packages.sh
@@ -16,6 +16,7 @@ sudo pacman -S fzf --noconfirm
 sudo pacman -S htop --noconfirm
 sudo pacman -S keychain --noconfirm
 sudo pacman -S lazygit --noconfirm
+sudo pacman -S lua --noconfirm
 sudo pacman -S mpv --noconfirm
 sudo pacman -S neovim --noconfirm
 sudo pacman -S nitrogen --noconfirm
