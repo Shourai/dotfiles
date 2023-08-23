177a9d213d1af88e063336f2f8ac9839e460edd2
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-23 08:38:26 +0200

    Add man package

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -1,6 +1,7 @@
 # Install supplementary packages
 sudo pacman -S chromium --noconfirm
 sudo pacman -S firefox --noconfirm
+sudo pacman -S man --noconfirm
 sudo pacman -S npm --noconfirm
 sudo pacman -S seahorse --noconfirm
 yay -S 1password --noconfirm
