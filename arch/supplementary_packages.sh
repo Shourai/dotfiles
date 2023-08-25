130e78bf4ef0d7ebf3a1a0377da633e62923d5c7
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-25 18:09:05 +0200

    Add buku, newsboat and syncthing

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -1,9 +1,12 @@
 # Install supplementary packages
+sudo pacman -S buku --noconfirm
 sudo pacman -S chromium --noconfirm
 sudo pacman -S firefox --noconfirm
 sudo pacman -S man --noconfirm
+sudo pacman -S newsboat --noconfirm
 sudo pacman -S npm --noconfirm
 sudo pacman -S seahorse --noconfirm
+sudo pacman -S syncthing --noconfirm
 yay -S 1password --noconfirm
 yay -S brave-bin --noconfirm
 yay -S dragon-drop-git --noconfirm
