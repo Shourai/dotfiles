e8def2af0007a916c44d2ec78fd93bd7fc521d7a
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-31 22:59:33 +0200

    Replace x11 packages with wayland packages

--- arch/packages.sh
+++ arch/packages.sh
@@ -7,21 +7,21 @@ makepkg -si --noconfirm
 sudo pacman -Syu --noconfirm
 
 # Install packages
+sudo pacman -S alacritty --noconfirm
 sudo pacman -S bat --noconfirm
-sudo pacman -S bspwm --noconfirm
 sudo pacman -S exa --noconfirm
 sudo pacman -S fd --noconfirm
-sudo pacman -S flameshot --noconfirm
 sudo pacman -S fzf --noconfirm
+sudo pacman -S grim --noconfirm
 sudo pacman -S htop --noconfirm
+sudo pacman -S hyprland --noconfirm
+sudo pacman -S hyprpaper --noconfirm
 sudo pacman -S lazygit --noconfirm
 sudo pacman -S lua --noconfirm
 sudo pacman -S mpv --noconfirm
 sudo pacman -S neovim --noconfirm
-sudo pacman -S nitrogen --noconfirm
 sudo pacman -S nnn --noconfirm
 sudo pacman -S openssh --noconfirm
-sudo pacman -S picom --noconfirm
 sudo pacman -S pipewire --noconfirm
 sudo pacman -S pipewire-pulse --noconfirm
 sudo pacman -S polkit --noconfirm
@@ -30,26 +30,18 @@ sudo pacman -S python3 --noconfirm
 sudo pacman -S rofi --noconfirm
 sudo pacman -S rofi-calc --noconfirm
 sudo pacman -S rsync --noconfirm
-sudo pacman -S sxhkd --noconfirm
+sudo pacman -S swayidle --noconfirm
+sudo pacman -S swaylock --noconfirm
 sudo pacman -S tmux --noconfirm
 sudo pacman -S tree --noconfirm
-sudo pacman -S xclip --noconfirm
-sudo pacman -S xorg-server --noconfirm
-sudo pacman -S xorg-xinit --noconfirm
+sudo pacman -S waybar --noconfirm
+sudo pacman -S wl-clipboard --noconfirm
 sudo pacman -S zathura-pdf-mupdf --noconfirm
 sudo pacman -S zsh --noconfirm
 sudo pacman -S zsh-autosuggestions --noconfirm
 sudo pacman -S zsh-syntax-highlighting --noconfirm
-yay -S betterlockscreen --noconfirm
-yay -S flashfocus --noconfirm
-yay -S nsxiv --noconfirm
-yay -S polybar --noconfirm
 yay -S shellcheck-bin --noconfirm
+yay -S tofi --noconfirm
 
-# Install terminal emulator
-git clone https://github.com/Shourai/st.git ~/Documents/github/st
-cd ~/Documents/github/st || exit
-git apply patches/*
-sudo make install
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
