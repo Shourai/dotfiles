29300ed8cd6ed8a3fdf400ca1706fd155b1c48ff
Shourai <10200748+Shourai@users.noreply.github.com>
2023-08-31 22:59:33 +0200

    Replace x11 packages with wayland packages

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -18,3 +18,24 @@ sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/get
 git clone https://git.suckless.org/tabbed ~/Documents/github/tabbed
 cd ~/Documents/github/tabbed || exit
 sudo make install
+
+# x11 packages
+#sudo pacman -S picom --noconfirm
+#sudo pacman -S nitrogen --noconfirm
+#sudo pacman -S flameshot --noconfirm
+#sudo pacman -S bspwm --noconfirm
+#sudo pacman -S sxhkd --noconfirm
+#sudo pacman -S xclip --noconfirm
+#sudo pacman -S xorg-server --noconfirm
+#sudo pacman -S xorg-xinit --noconfirm
+#yay -S betterlockscreen --noconfirm
+#yay -S flashfocus --noconfirm
+#yay -S nsxiv --noconfirm
+#yay -S polybar --noconfirm
+
+
+# Install terminal emulator
+#git clone https://github.com/Shourai/st.git ~/Documents/github/st
+#cd ~/Documents/github/st || exit
+#git apply patches/*
+#sudo make install
