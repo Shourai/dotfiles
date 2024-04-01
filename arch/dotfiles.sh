d3dc1cf9e05d6adba863b056a156d596659eec99
Shourai <10200748+Shourai@users.noreply.github.com>
2024-04-01 15:37:04 +0200

    Update dotfiles for wayland

--- arch/dotfiles.sh
+++ arch/dotfiles.sh
@@ -11,19 +11,31 @@ ln -sf ~/Documents/github/dotfiles/tmux/tmux.conf ~/.config/tmux
 ln -sf ~/Documents/github/dotfiles/git ~/.config
 
 if [ "$(uname)" = "Linux" ]; then
-    ln -sf ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
-    ln -sf ~/Documents/github/dotfiles/arch/picom.conf ~/.config
-    ln -sf ~/Documents/github/dotfiles/arch/bspwm ~/.config
-    ln -sf ~/Documents/github/dotfiles/arch/sxhkd ~/.config
-    ln -sf ~/Documents/github/dotfiles/arch/polybar ~/.config
-    ln -sf ~/Documents/github/dotfiles/arch/rofi ~/.config
+    rm ~/.config/hypr
+    ln -sf ~/Documents/github/dotfiles/arch/wayland/alacritty ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/wayland/hypr ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/wayland/swayidle ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/wayland/swaylock ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/wayland/waybar ~/.config
+    ln -sf ~/Documents/github/dotfiles/arch/wayland/wofi ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/fontconfig ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/fonts ~/.local/share
-    ln -sf ~/Documents/github/dotfiles/arch/bspwm/scripts/bspwm_resize ~/.local/bin
 fi
 
-if [ -f /etc/arch-release ]; then
-    # Install x-server keyboard-layout
-    sudo rsync -a ~/Documents/github/dotfiles/arch/20-keyboard-layout.conf /etc/X11/xorg.conf.d/
-fi
+#if [ "$(uname)" = "Linux" ]; then
+#    ln -sf ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
+#    ln -sf ~/Documents/github/dotfiles/arch/picom.conf ~/.config
+#    ln -sf ~/Documents/github/dotfiles/arch/bspwm ~/.config
+#    ln -sf ~/Documents/github/dotfiles/arch/sxhkd ~/.config
+#    ln -sf ~/Documents/github/dotfiles/arch/polybar ~/.config
+#    ln -sf ~/Documents/github/dotfiles/arch/rofi ~/.config
+#    ln -sf ~/Documents/github/dotfiles/arch/fontconfig ~/.config
+#    ln -sf ~/Documents/github/dotfiles/arch/fonts ~/.local/share
+#    ln -sf ~/Documents/github/dotfiles/arch/bspwm/scripts/bspwm_resize ~/.local/bin
+#fi
+
+#if [ -f /etc/arch-release ]; then
+#    # Install x-server keyboard-layout
+#    sudo rsync -a ~/Documents/github/dotfiles/arch/20-keyboard-layout.conf /etc/X11/xorg.conf.d/
+#fi
 
