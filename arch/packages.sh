e3a75a8ba9135ffdeb0c6efd397f7cb8807f7278
Shourai <10200748+Shourai@users.noreply.github.com>
2024-04-01 15:36:50 +0200

    Update packages

--- arch/packages.sh
+++ arch/packages.sh
@@ -9,7 +9,7 @@ sudo pacman -Syu --noconfirm
 # Install packages
 sudo pacman -S alacritty --noconfirm
 sudo pacman -S bat --noconfirm
-sudo pacman -S exa --noconfirm
+sudo pacman -S eza --noconfirm
 sudo pacman -S fd --noconfirm
 sudo pacman -S fzf --noconfirm
 sudo pacman -S grim --noconfirm
@@ -21,14 +21,13 @@ sudo pacman -S lua --noconfirm
 sudo pacman -S mpv --noconfirm
 sudo pacman -S neovim --noconfirm
 sudo pacman -S nnn --noconfirm
+sudo pacman -S npm --noconfirm
 sudo pacman -S openssh --noconfirm
 sudo pacman -S pipewire --noconfirm
 sudo pacman -S pipewire-pulse --noconfirm
 sudo pacman -S polkit --noconfirm
 sudo pacman -S python-pip --noconfirm
 sudo pacman -S python3 --noconfirm
-sudo pacman -S rofi --noconfirm
-sudo pacman -S rofi-calc --noconfirm
 sudo pacman -S rsync --noconfirm
 sudo pacman -S slurp --noconfirm
 sudo pacman -S swayidle --noconfirm
@@ -37,12 +36,12 @@ sudo pacman -S tmux --noconfirm
 sudo pacman -S tree --noconfirm
 sudo pacman -S waybar --noconfirm
 sudo pacman -S wl-clipboard --noconfirm
+sudo pacman -S wofi --noconfirm
 sudo pacman -S zathura-pdf-mupdf --noconfirm
 sudo pacman -S zsh --noconfirm
 sudo pacman -S zsh-autosuggestions --noconfirm
 sudo pacman -S zsh-syntax-highlighting --noconfirm
 yay -S shellcheck-bin --noconfirm
 yay -S tofi --noconfirm
-
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
