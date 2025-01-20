20b5ac92ac99b2cd0b30338510892281432c65d5
Shourai <10200748+Shourai@users.noreply.github.com>
2025-01-20 10:18:18 +0100

    Use yazi in favour of nnn

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -3,20 +3,19 @@ sudo pacman -S chromium --noconfirm
 sudo pacman -S firefox-developer-edition --noconfirm
 sudo pacman -S man --noconfirm
 sudo pacman -S newsboat --noconfirm
-sudo pacman -S npm --noconfirm
 sudo pacman -S syncthing --noconfirm
 yay -S 1password --noconfirm
 yay -S brave-bin --noconfirm
 yay -S buku --noconfirm
 yay -S dragon-drop-git --noconfirm
 
-# Add nnn plugins
-sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
-
-# Install tabbed for nnn tabbed to work
-git clone https://git.suckless.org/tabbed ~/Documents/github/tabbed
-cd ~/Documents/github/tabbed || exit
-sudo make install
+## Add nnn plugins
+#sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+#
+## Install tabbed for nnn tabbed to work
+#git clone https://git.suckless.org/tabbed ~/Documents/github/tabbed
+#cd ~/Documents/github/tabbed || exit
+#sudo make install
 
 # x11 packages
 #sudo pacman -S picom --noconfirm
