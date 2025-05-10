c5f4b3f1c8f64da2c07e8feee3fdc474fbfbfd41
Shourai <10200748+Shourai@users.noreply.github.com>
2025-05-10 17:42:50 +0200

    Update dotfiles symlink script

--- arch/dotfiles.sh
+++ arch/dotfiles.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.local/share
 ln -sf ~/Documents/github/dotfiles/zsh ~/.config/zsh
 
 ln -sf ~/Documents/github/nvim ~/.config/nvim
-ln -sf ~/Documents/github/dotfiles/tmux/tmux.conf ~/.config/tmux.conf
+ln -sf ~/Documents/github/dotfiles/tmux ~/.config
 ln -sf ~/Documents/github/dotfiles/git ~/.config
 ln -sf ~/Documents/github/dotfiles/yazi ~/.config
 
@@ -22,21 +22,3 @@ if [ "$(uname)" = "Linux" ]; then
     ln -sf ~/Documents/github/dotfiles/arch/fontconfig ~/.config
     ln -sf ~/Documents/github/dotfiles/arch/fonts ~/.local/share
 fi
-
-#if [ "$(uname)" = "Linux" ]; then
-#    ln -sf ~/Documents/github/dotfiles/arch/xinitrc ~/.xinitrc
-#    ln -sf ~/Documents/github/dotfiles/arch/picom.conf ~/.config
-#    ln -sf ~/Documents/github/dotfiles/arch/bspwm ~/.config
-#    ln -sf ~/Documents/github/dotfiles/arch/sxhkd ~/.config
-#    ln -sf ~/Documents/github/dotfiles/arch/polybar ~/.config
-#    ln -sf ~/Documents/github/dotfiles/arch/rofi ~/.config
-#    ln -sf ~/Documents/github/dotfiles/arch/fontconfig ~/.config
-#    ln -sf ~/Documents/github/dotfiles/arch/fonts ~/.local/share
-#    ln -sf ~/Documents/github/dotfiles/arch/bspwm/scripts/bspwm_resize ~/.local/bin
-#fi
-
-#if [ -f /etc/arch-release ]; then
-#    # Install x-server keyboard-layout
-#    sudo rsync -a ~/Documents/github/dotfiles/arch/20-keyboard-layout.conf /etc/X11/xorg.conf.d/
-#fi
-
