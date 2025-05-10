68775d95753fdec551f3fe61ab44345166231c96
Shourai <10200748+Shourai@users.noreply.github.com>
2025-05-10 22:55:32 +0200

    Add zoxide to packages

--- arch/supplementary_packages.sh
+++ arch/supplementary_packages.sh
@@ -2,9 +2,9 @@
 sudo pacman -S chromium --noconfirm
 sudo pacman -S firefox-developer-edition --noconfirm
 sudo pacman -S man --noconfirm
-sudo pacman -S newsboat --noconfirm
 sudo pacman -S syncthing --noconfirm
 yay -S 1password --noconfirm
 yay -S brave-bin --noconfirm
 yay -S buku --noconfirm
 #yay -S dragon-drop-git --noconfirm
+#sudo pacman -S newsboat --noconfirm
